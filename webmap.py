7d2b7ecdae486abafada034d68f9799642d2b9cb
naman10110105 <namanvashishth12@gmail.com>
2020-07-11 00:20:08 +0530

    version 1

---
+++ webmap.py
@@ -0,0 +1,12 @@
+from geopy.geocoders import Nominatim
+import folium
+import webbrowser
+
+location = input("Enter location: ")
+nom = Nominatim(user_agent="map")
+map_form = folium.Map(location=[nom.geocode(location).latitude, nom.geocode(
+    location).longitude], zoom_start=12, tiles='Stamen Terrain')
+folium.Marker(location=[nom.geocode(location).latitude, nom.geocode(location).longitude], popup=nom.geocode(
+    location).address, icon=folium.Icon(color='red')).add_to(map_form)
+map_form.save("Map.html")
+webbrowser.open_new_tab("Map.html")
