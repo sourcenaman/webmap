16cf2717e924fd24512bfa40901238fb6da9889b
naman10110105 <namanvashishth12@gmail.com>
2020-07-11 11:15:16 +0530

    Layer Control

--- webmap.py
+++ webmap.py
@@ -9,6 +9,7 @@ map_form = folium.Map(location=[nom.geocode(location).latitude, nom.geocode(
 folium.Marker(location=[nom.geocode(location).latitude, nom.geocode(location).longitude], popup=nom.geocode(
     location).address, icon=folium.Icon(color='red')).add_to(map_form)
 folium.GeoJson(
-    data=(open('world.json', 'r', encoding='utf-8-sig').read())).add_to(map_form)
+    data=(open('world.json', 'r', encoding='utf-8-sig').read()), name="Border").add_to(map_form)
+folium.LayerControl().add_to(map_form)
 map_form.save("Map.html")
 webbrowser.open_new_tab("Map.html")
