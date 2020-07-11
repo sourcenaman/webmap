976e559b8443744c48975c8a3b461054ff250b24
naman10110105 <namanvashishth12@gmail.com>
2020-07-11 11:09:56 +0530

    country borders

--- webmap.py
+++ webmap.py
@@ -8,5 +8,7 @@ map_form = folium.Map(location=[nom.geocode(location).latitude, nom.geocode(
     location).longitude], zoom_start=12, tiles='Stamen Terrain')
 folium.Marker(location=[nom.geocode(location).latitude, nom.geocode(location).longitude], popup=nom.geocode(
     location).address, icon=folium.Icon(color='red')).add_to(map_form)
+folium.GeoJson(
+    data=(open('world.json', 'r', encoding='utf-8-sig').read())).add_to(map_form)
 map_form.save("Map.html")
 webbrowser.open_new_tab("Map.html")
