cc03058b5da951e2c2ccaeb7653a8ffc9ebbfcd2
naman10110105 <namanvashishth12@gmail.com>
2020-07-14 20:25:04 +0530

    zoom change

--- webmap.py
+++ webmap.py
@@ -5,7 +5,7 @@ import webbrowser
 location = input("Enter location: ")
 nom = Nominatim(user_agent="map")
 map_form = folium.Map(location=[nom.geocode(location).latitude, nom.geocode(
-    location).longitude], zoom_start=12, tiles='Stamen Terrain')
+    location).longitude], zoom_start=20, tiles='Stamen Terrain')
 folium.Marker(location=[nom.geocode(location).latitude, nom.geocode(location).longitude], popup=nom.geocode(
     location).address, icon=folium.Icon(color='red')).add_to(map_form)
 folium.GeoJson(
